dbbd15790fe7df3b2a6190b402b5f2560a125948
OoWaterMelons <15884455616@163.com>
2020-05-30 08:41:43 +0800

    获取到符合规则的pageid,还没有进行统计分析

--- src/main/java/com/imooc/bigdata/hadoop/mr/project/utils/ContentUtils.java
+++ src/main/java/com/imooc/bigdata/hadoop/mr/project/utils/ContentUtils.java
@@ -9,21 +9,20 @@ import java.util.regex.Pattern;
  * @author EaApple
  * @5/28/2020 9:49 PM
  * description： 用于统计页面流量  拿到pageid
+ * 核心在于使用模式匹配和正则表达式的组合使用
  */
 public class ContentUtils {
 
     public static String getPageId(String url){
         String pageId = "";
-        if(StringUtils.isBlank(url)){
-            return pageId;
-        }
+        if(StringUtils.isNotBlank(url)){
+            // 模式匹配代码  重要复习
+            Pattern pattern = Pattern.compile("topicId=[0-9]+");
+            Matcher matcher = pattern.matcher(url);
 
-        // 模式匹配代码  重要复习
-        Pattern pattern = Pattern.compile("topicId=[0-9]+");
-        Matcher matcher = pattern.matcher(url);
-
-        if(matcher.find()){
-            pageId = matcher.group().split("topicId=")[1];
+            if(matcher.find()){
+                pageId = matcher.group().split("topicId=")[1];
+            }
         }
         return pageId;
     }
--- src/test/java/com/imooc/bigdata/hadoop/mr/project/utils/LogParseTest.java
+++ src/test/java/com/imooc/bigdata/hadoop/mr/project/utils/LogParseTest.java
@@ -38,5 +38,4 @@ public class LogParseTest {
         System.out.println(pageId);
     }
 
-
 }
